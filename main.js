47510c0a608c5d45868fa9a132ad438fe9546034
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-16 14:52:32 +0200

    get help ticket

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-for (let index = 0; index < 100; index++) {
+const row = document.querySelector('row-cols-10');
+console.log(row);
+
+for (let index = 1; index <= 100; index++) {
     
     if (index % 3 == 0 && index % 5 == 0) {
+    const colaggiunta = document.createElement('div');
+    colaggiunta.classList.add('col');
+    colaggiunta.append(index);
+    row.append(colaggiunta);
     console.log("fizzbuzz");
+    colaggiunta.innerHTML += "fizzbuzz"
         }else if (index % 3 == 0){
             console.log("fizz");
             }else if (index % 5 == 0){
                 console.log("buzz");
-            } else{
-                console.log(index);
-            }
-    
+                } else{
+                    console.log(index);
+                }  
 }
+
