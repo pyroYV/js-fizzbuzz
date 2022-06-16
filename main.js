1bed84e901c0f6154963d9da46e1640be635b51f
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-16 15:22:52 +0200

    bonus 1 e 2

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-const row = document.querySelector('row-cols-10');
-console.log(row);
+const row = document.querySelector('.row');
 
 for (let index = 1; index <= 100; index++) {
-    
+    const colAggiunta = document.createElement('div');
+    colAggiunta.classList.add('col-4','text-center','text-white','rounded');
+    row.append(colAggiunta);
     if (index % 3 == 0 && index % 5 == 0) {
-    const colaggiunta = document.createElement('div');
-    colaggiunta.classList.add('col');
-    colaggiunta.append(index);
-    row.append(colaggiunta);
+    
     console.log("fizzbuzz");
-    colaggiunta.innerHTML += "fizzbuzz"
+    colAggiunta.innerHTML += "fizzbuzz"
+    colAggiunta.classList.add('bg-secondary');
         }else if (index % 3 == 0){
             console.log("fizz");
+            colAggiunta.innerHTML ="fizz";
+            colAggiunta.classList.add('bg-primary');
             }else if (index % 5 == 0){
                 console.log("buzz");
+                colAggiunta.innerHTML ="buzz";
+                colAggiunta.classList.add('bg-danger');
                 } else{
                     console.log(index);
+                    colAggiunta.innerHTML = index ;
+                    colAggiunta.classList.add('bg-dark');
                 }  
 }
 
